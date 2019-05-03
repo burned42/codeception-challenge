f4ebe3a7dc31598ae2f45161d2e690e27d73267f
Bernd Stellwag <burned@zerties.org>
2019-05-03 18:59:47 +0200

    add some classes as starting point for taking the coding challenge

---
+++ src/Dice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Dice
+{
+    public function roll(): int
+    {
+    }
+}
---
+++ src/DiceGame.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class DiceGame
+{
+    public function __construct(array $dice)
+    {
+    }
+
+    public function score(): int
+    {
+    }
+}
